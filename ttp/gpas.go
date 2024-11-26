7c202c1b789cfd19f519b24a37667da18386764e
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-11-26 14:59:44 +0100

    add domain struct

---
+++ ttp/gpas.go
@@ -0,0 +1,10 @@
+package ttp
+
+import "encoding/xml"
+
+type domain struct {
+	XMLName xml.Name `xml:"plant"`
+	Id      int      `xml:"id,attr"`
+	Name    string   `xml:"name"`
+	Origin  []string `xml:"origin"`
+}
